b464a3fcdec563db902d707900341e42c3ae5748
Kenneth Ng <ng.tszcheong@gmail.com>
2018-06-06 17:54:53 +0800

    Add links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 // @flow
 
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Button, Layout, List } from 'antd'
 import './App.css'
 
-type Props = {
-}
+const listItems = [
+  {
+    title: 'React project: Instagram feed',
+    url: 'https://github.com/kennethnghk/instagram-feeder'
+  }
+]
+
+type Props = {}
 
 class App extends Component<Props> {
   render () {
+    const { Header, Footer, Sider, Content } = Layout
     return (
-      <div className='App'>
-        <Button type='primary'>Button</Button>
-      </div>
+      <Layout>
+        <Header>Kenlab.me</Header>
+        <List
+          bordered
+          dataSource={listItems}
+          renderItem={item => (
+            <List.Item>
+              <a href={item.url}>{item.title}</a>
+            </List.Item>
+          )}
+        />
+      </Layout>
     )
   }
 }
