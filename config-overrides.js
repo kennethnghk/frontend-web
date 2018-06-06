4bec0429b24af76444f6856a3c3b867bce67c1b8
Kenneth Ng <ng.tszcheong@gmail.com>
2018-06-06 17:18:34 +0800

    add prettier-standard with precommit hook

--- config-overrides.js
+++ config-overrides.js
@@ -2,5 +2,8 @@ const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+
+
+    
     return config;
   };
--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Button } from 'antd';
-import './App.css';
+import React, { Component } from 'react'
+import { Button } from 'antd'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
+      <div className='App'>
+        <Button type='primary'>Button</Button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
