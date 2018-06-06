7cd6319c0b977f7c10e3a29bc12a6fbc462ec1bf
Kenneth Ng <ng.tszcheong@gmail.com>
2018-06-06 17:21:56 +0800

    Apply prettier-format to all JS files

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,10 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const { injectBabelPlugin } = require('react-app-rewired')
 
-module.exports = function override(config, env) {
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+module.exports = function override (config, env) {
+  config = injectBabelPlugin(
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+    config
+  )
 
-
-    
-    return config;
-  };
+  return config
+}
